58dbc5f69a7181e5e5d5eb00fecd001d9b2c8462
root <root@s052d7ba7.fastvps-server.com>
2017-04-04 21:47:31 +0300

    env

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
             'username' => 'root',
-            'password' => '',
+            'password' => '321',
             'charset' => 'utf8',
         ],
         'mailer' => [
